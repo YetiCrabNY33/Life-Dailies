4749d3b7b07794a0d98c1e7eb02586f2562e90a9
Jordan <jlong009@outlook.com>
2022-05-30 21:45:29 -0400

    Created quest handler and expanded server files

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express(); //indicates this is the server
 const PORT = 3000;
+const questHandler = require('./questHandler');
+const bodyParser = require('body-parser');
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true})); // needed for POST requests which send data through the body using 'x-www-form-urlencoded' format
+app.use(express.json()); // needed to parse JSON objects that are sent through the server
 
+app.post('/addQuest/', //this quest is for 
+  questHandler.addQuest,
+  (req, res) => {
+    res.status(200).send(); // this should send a confirmation that a quest was added
+    // I'm not sure how to do this
+});
 
+app.get('/questlist/',  //get request handler to retrieve the list of quests
+  questHandler.questList,
+  (req, res) => {
+    res.status(200).send(); 
+});
+
+//error handler in case client navigated away from used domains
 app.use((req, res)=> {
   return res.status(404).send('You in the wrong place!')
 });
